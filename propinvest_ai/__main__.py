62636275ad3e343b786206a396323768dc1ffa95
ada0l <andreika.varfolomeev@yandex.ru>
2023-12-03 16:38:56 +1000

    update

--- propinvest_ai/__main__.py
+++ propinvest_ai/__main__.py
@@ -1,10 +1,27 @@
-from fastapi import FastAPI
+import json
 
+import requests
+from fastapi import BackgroundTasks, FastAPI, Request
+from pydantic import Json
+
+from propinvest_ai.chat import get_answer
 from propinvest_ai.settings import settings
 
 app = FastAPI()
 
 
-@app.get("/")
-def ping():
+def send_answer(req_id: str, payload: dict):
+    data = get_answer(json.dumps(payload))
+    try:
+        requests.post(settings.web_hook, json={"req_id": req_id, "giga": data})
+    except Exception as ex:
+        print(ex)
+
+
+@app.post("/webhook/{req_id}")
+async def webhook(
+    req_id: str, req: Request, background_tasks: BackgroundTasks
+):
+    data = json.loads(json.dumps(await req.json(), ensure_ascii=False))
+    background_tasks.add_task(send_answer, req_id, data)
     return dict(message="pong")
--- propinvest_ai/settings.py
+++ propinvest_ai/settings.py
@@ -4,6 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     giga_token: str
     giga_scope: str
+    web_hook: str
 
 
 settings = Settings()  # type: ignore
